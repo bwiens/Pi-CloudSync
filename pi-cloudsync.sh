950c83b69bd36bb17a35ca59f5531ecb7654c384
BHES <root@localhost.(none)>
2014-03-19 00:32:33 +0100

    ..

---
+++ pi-cloudsync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Authors Benjamin Wiens & BHES
+
+# Variables
+Remotehost=""
+Remoteport=""
+Remotedir=""
+Homedir=""
+User=""
+Logfile=""
+Email=""
+Emailfile=""
+
+# Start logging.
+echo "Pi-CloudSync started at $(date) by $(whoami)" >> $Logfile;
+
+# Start RSYNC.
+rsync -rvz -e 'ssh -p $Remoteport' --progress $Homedir $User@$Remotehost:$Remotedir;
